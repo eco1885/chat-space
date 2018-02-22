2bb149286de4d11c9be1c8ed1a048b35e851767a
Masaki Taniguchi <36178714+eco1885@users.noreply.github.com>
2018-02-22 21:58:22 +0900

    Revert "group_function_controller_model_create"

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'messages#index'
-  resources :user, only: [:edit, :update]
-  resources :groups, only: [:new, :create, :edit, :update] do
-    resources :messages, only: [:index, :create]
-  end
+  resource :user, only: [:edit, :update]
 end

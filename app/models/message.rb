8aa290dd4ed4ddddfa336fc6284b92288e9a83a4
谷口晶紀 <owner@taniguchiaki-no-MacBook-Pro.local>
2018-02-23 11:49:59 +0900

    Revert "chat-space-Asynchronous_communication_scroll"
    This reverts commit 719052af7a07bd6239a4b676e145b4413a9a063e.

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,14 +2,7 @@ class Message < ApplicationRecord
   belongs_to :group
   belongs_to :user
 
-  # validates :content, presence: true, unless: :image?
-  validates :content_image, presence: true
+  validates :content, presence: true, unless: :image?
   mount_uploader :image, ImageUploader
-
-  private
-
-  def content_image
-    content.presence or image.presence
-  end
 
 end

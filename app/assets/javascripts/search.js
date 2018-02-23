e8ba2ce3e42a077de756957d3eb1cd8cbe5c1b69
谷口晶紀 <owner@taniguchiaki-no-MacBook-Pro.local>
2018-02-23 17:18:40 +0900

    incrimenatal_search_Asynchronous_communication

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -2,5 +2,15 @@ $(function() {
   $("#user-search-field").on("keyup", function() {
     var input = $("#user-search-field").val();
     console.log(input)
+
+    $.ajax({
+      type: 'GET',
+      url: '/users',
+      data: { name: input },
+      dataType: 'json'
+    })
+    .done(function(data){
+      console.log(data);
+    })
   });
 });

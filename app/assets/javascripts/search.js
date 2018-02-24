f48314053864c1094671ef94fe804548d39d5085
谷口晶紀 <owner@taniguchiaki-no-MacBook-Pro.local>
2018-02-24 11:34:08 +0900

    incrimental_search_addfunction2

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -44,10 +44,22 @@ $(function() {
 //ユーザー追加機能
 $(function(){
 
+  function chatMember(user_id,user_name){
+    var memberHTML =
+      `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
+        <input name='group[user_ids][]' type='hidden' value=${user_id}>
+        <p class='chat-group-user__name'>${user_name}</p>
+        <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
+      </div>`
+      $("#chat-group-user-22").append(memberHTML)
+  }
 
     $("#user-search-result").on("click", ".chat-group-user__btn--add", function(){
+      var user_id = $(this).data("user-id");
+      var user_name = $(this).data("user-name")
       console.log(this)
+      $(this).parent().remove();
+      chatMember(user_id,user_name)
     });
-
 
 });

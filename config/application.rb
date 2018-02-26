36b4feb5245d0b48de7c60ffeea8a96d05a40787
谷口晶紀 <owner@taniguchiaki-no-MacBook-Pro.local>
2018-02-26 20:53:37 +0900

    time_zone_delete

--- config/application.rb
+++ config/application.rb
@@ -6,9 +6,6 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-config.time_zone = 'Tokyo'
-config.active_record.default_timezone = :local
-
 module ChatSpace
   class Application < Rails::Application
     config.generators do |g|

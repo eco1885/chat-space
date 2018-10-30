c7adc798deed3d8810d7f90b86139ecb711c2c64
eco1885 <taniguchi1201@hotmail.co.jp>
2018-10-30 21:08:29 +0900

    記述場所の変更　

--- config/application.rb
+++ config/application.rb
@@ -19,8 +19,7 @@ module ChatSpace
     config.i18n.default_locale = :ja
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
+    config.assets.initialize_on_precompile = false
 
   end
 end
-
-config.assets.initialize_on_precompile = false
